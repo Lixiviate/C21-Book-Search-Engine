f312a5df611857cb1f80d4cd02d34f2f5dacc3f5
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-10-04 19:43:20 -0600

    Setup mutations and queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,20 @@
 import { gql } from "@apollo/client";
 
-export const GET_ME = gql``;
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
